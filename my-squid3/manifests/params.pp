d3e1109364b69762b20907a2fb5d4d969a1724d9
root <root@puppetserver>
2016-02-25 16:03:37 -0500

    Version 2.1 with OpenBSD support

--- my-squid3/manifests/params.pp
+++ my-squid3/manifests/params.pp
@@ -16,6 +16,13 @@ $service_name  =  'squid'
 $service_dir   =  '/etc/squid/'
 $service_file  =  '/etc/squid/squid.conf'
 }
+'OpenBSD': {
+$package_name  = 'squid'
+$package_version = '3.5.6'
+$service_name  =  'squid'
+$service_dir   =  '/etc/squid/'
+$service_file  =  '/etc/squid/squid.conf'
+}
 
 default: {
 $package_name  = 'squid'
